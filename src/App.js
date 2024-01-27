a3885e2a4c30a6dac9120c37d53c4de5c52939de
han18 <han.fadah@gmail.com>
2024-01-27 15:56:19 -0500

    consructed the props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { learnerData } from "./data/learnerData";
 import { useState } from "react";
 import "./App.css";
-import Learner from "./Components/Learner";
+import LearnerComponent from "./Components/Learner";
 
 function App() {
   const [learners, setLearners] = useState(learnerData);
@@ -9,18 +9,7 @@ function App() {
   return (
     <div>
       <h1>Learner Data:</h1>
-
-      {learners.map((learner) => (
-        <h2 key={learner.id}>
-          Name: {learner.name}
-          <br></br>
-          Bio: {learner.bio}
-          Scores: {learner.scores.data}
-        </h2>
-      ))}
-      {learners.scores.data.map((learner, next) => {
-        <h key={next}>Scores: {learner}</h>;
-      })}
+      <LearnerComponent learners={learners} />
     </div>
   );
 }
--- src/Components/Learner.js
+++ src/Components/Learner.js
@@ -1,9 +1,27 @@
-function Learner(learners) {
+// import React, { useState } from "react";
+// import { learnerData } from "../data/learnerData";
+
+const LearnerComponent = (props) => {
+  const learners = props.learners;
   return (
     <div>
-      <h1>Name: {learners.name} </h1>
+      <h1>Learners</h1>
+      {learners.map((learner) => (
+        <div key={learner.id}>
+          <h2>{learner.name}</h2>
+          <p>{learner.bio}</p>
+          <h3>Scores:</h3>
+          <ul>
+            {learner.scores.map((score, index) => (
+              <li key={index}>
+                Date: {score.date}, Score: {score.score}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default Learner;
+export default LearnerComponent;
